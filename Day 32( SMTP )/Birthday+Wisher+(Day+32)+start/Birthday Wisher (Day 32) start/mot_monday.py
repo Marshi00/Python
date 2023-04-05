8880b7fea718fe66acb97b7d2587d8cd00509a16
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2023-04-04 20:02:46 -0400

    Update mot_monday.py

--- Day 32( SMTP )/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/mot_monday.py
+++ Day 32( SMTP )/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/mot_monday.py
@@ -9,13 +9,13 @@ if now.weekday() == 0:
         quotes_list = quotes.readlines()
         mot_quote = random.choice(quotes_list)
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        my_email = "mo.arshian00@gmail.com"
+        my_email = ""
         # make an app password in your Google account, and it is different from normal pass , 2 step must be active
-        my_password = "nmlhyatkgswntemn"
+        my_password = ""
         connection.starttls()
         connection.login(user=my_email, password=my_password)
         connection.sendmail(from_addr=my_email,
-                            to_addrs="arshiyan.mohammad48@gmail.com",
+                            to_addrs="",
                             msg=f"Subject:Hello\n\n {mot_quote}")
 
 
